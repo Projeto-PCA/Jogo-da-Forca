3471d91431b34b56662d932ea096494153026f6e
Welington da Silva Andrade <Welingtonzin@gmail.com>
2021-05-26 09:31:48 -0300

    Resolução do primeiro bug adicionando o .upper() na entrada.

--- inicio.py
+++ inicio.py
@@ -1,8 +1,11 @@
-resposta = 'chave'
-
-letra = input('Insira a letra da Palavra Oculta :')
-
+#Adicionado .	upper para remover bug na entrada do usuario
+resposta = 'chave'.upper()
+# Entrada Do Usuário
+# 
+letra = input('Insira a letra da Palavra Oculta : ').upper()
+# Condicional de uma Letra na variavel Resposta
+# 
 if letra in resposta:
-	print('Você acertou a letra');
+	print('Você acertou a Letra')
 else:
 	print('Voce errou a Letra')
