923b53f70c12e0920698f0aa58714ad452871ba1
Welington da Silva Andrade <Welingtonzin@gmail.com>
2021-05-26 11:57:00 -0300

    Correção no esboço do metodo while e edição da estrutura condicional.

--- inicio.py
+++ inicio.py
@@ -1,18 +1,30 @@
-#Adicionado .	upper para remover bug na entrada do usuario
+#Adicionado    .upper() para remover bug na entrada do usuario.
 resposta = 'chave'.upper()
-# Entrada Do Usuário
-# 
-letra = input('Insira a letra da Palavra Oculta : ').upper()
-# Condicional de uma Letra na variavel Resposta
-# 
-if letra in resposta:
-	print('Você acertou a Letra')
-else:
-	print('Voce errou a Letra')
-	#Criada estrutura de repetição
-	entrada = input('Digite p para pausar o Programa!!')
-	while entrada != 'p':
-		print('voce entrou no loop')
+acertou = 0
+errou = 0
+letras_certas = ''
+letras_erradas = ''
+
+#Estrutura de repetição para continuar o progama.
+while acertou != len(resposta) and errou != 5:
+	# Entrada Do Usuário.
+
+	letra = input('Insira a letra da Palavra Oculta : ').upper()
+	print('Voce ja acertou essas letras : ' + letras_certas)
+	print('Voce ja errou essas letras : ' + letras_erradas)
+
+# Condicional de uma Letra na variavel Resposta.
+
+	if letra in resposta:
+		print('Você acertou a Letra!!')
+		letras_certas += letra
+		acertou += 1
+	else:
+		print('Você errou a Letra!!')
+		letras_erradas += letra
+		errou += 1
+
+
 
 
 
