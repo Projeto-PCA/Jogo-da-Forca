7f6127eb1bb90fead2007b6cbf0caf334739d286
Welington da Silva Andrade <Welingtonzin@gmail.com>
2021-05-26 15:19:06 -0300

    Implementação de saida da palavra secreta

--- inicio.py
+++ inicio.py
@@ -5,22 +5,30 @@ errou = 0
 letras_certas = ''
 letras_erradas = ''
 
+
+
 #Estrutura de repetição para continuar o progama.
 while acertou != len(resposta) and errou != 5:
+	mensagem = ''
+	for letra in resposta:
+		if letra in letras_certas:
+			mensagem += letra + ' '
+		else:
+			mensagem += '_ '
+	print(mensagem)
 	# Entrada Do Usuário.
 
 	letra = input('Insira a letra da Palavra Oculta : ').upper()
-	print('Voce ja acertou essas letras : ' + letras_certas)
 	print('Voce ja errou essas letras : ' + letras_erradas)
 
-# Condicional de uma Letra na variavel Resposta.
+# Condicional de uma Letra na Resposta
 
 	if letra in resposta:
-		print('Você acertou a Letra!!')
+		print('Voce acertou a Letra: '+ letra)
 		letras_certas += letra
 		acertou += 1
 	else:
-		print('Você errou a Letra!!')
+		print('Voce errou a Letra: ' + letra)
 		letras_erradas += letra
 		errou += 1
 
